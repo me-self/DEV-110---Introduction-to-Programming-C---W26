f0823b57d9ba4122e4c99e0a1f71cf160d348169
Samuel <samuel.bellemare@bellevuecollege.edu>
2026-03-26 15:50:47 -0700

    Implement more ratio calculations

--- final-project/BodyBuddy/Ratios.cs
+++ final-project/BodyBuddy/Ratios.cs
@@ -1,6 +1,6 @@
 public static class Ratios
 {
-    public static float? FallibleRatio(float? a, float? b, Func<float, float, float> ratioFunc)
+    private static double? FallibleRatio(double? a, double? b, Func<double, double, double> ratioFunc)
     {
         // Return null if missing a value, otherwise calculate and return the ratio.
         bool incomplete = !(a.HasValue && b.HasValue);
@@ -8,14 +8,35 @@ public static class Ratios
         return incomplete ? null : ratioFunc(a!.Value, b!.Value);
     }
 
-    public static float? GetBmi(float? weight, float? height)
+    public static double? GetBmi(double? weight, double? height)
     {
         const int WEIGHT_MULTIPLIER = 703;
         return FallibleRatio(weight, height, (w, h) => w * WEIGHT_MULTIPLIER / (h * h));
     }
 
-    public static float? GetBri(float? height, float? waist)
+    public static double? GetWhtr(double? height, double? waist)
     {
-        return FallibleRatio(height, waist, (h, w) => 0); // TODO: Implement BRI calculation.
+        return FallibleRatio(waist, height, (w, h) => w / h);
+    }
+
+    public static double? GetWhr(double? waist, double? hip)
+    {
+        return FallibleRatio(waist, hip, (w, h) => w / h);
+    }
+
+    public static double? GetApeIndex(double? height, double? wingspan)
+    {
+        return FallibleRatio(height, wingspan, (h, w) => w / h);
+    }
+
+    public static double? GetBri(double? height, double? waist)
+    {
+        return FallibleRatio(waist, height, (w, h) =>
+        {
+            // `h` and `w` aren't null so this is safe to cast.
+            double whtr = (double)GetWhtr(h, w);
+            double eccentricity = double.Sqrt(1 - double.Pow(whtr / Math.PI, 2));
+            return 364.2f - 365.5f * eccentricity;
+        });
     }
 }
